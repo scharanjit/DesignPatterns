b42a0648597529e94783dcb7c8aafdf998213462
Charanjit Singh <v-charanjit.s@turvo.com>
2018-06-08 16:14:29 +0530

    C

--- src/Java8/PreventCollectionFrmWastingMemory.java
+++ src/Java8/PreventCollectionFrmWastingMemory.java
@@ -5,23 +5,40 @@ import java.util.List;
 
 public class PreventCollectionFrmWastingMemory {
 
-    static List<Integer> list = new ArrayList<>();
-    public static void main(String[] args) {
-        int element =1;
 
-        addToList(element);
+    // *** Old code ***
+   static private List<Foo> list = new ArrayList<>();
+    static void addToListOld(Foo foo) { list.add(foo); }
+    // *** New code ***
+    // If list is empty, this is null. If list contains only one element,
+    // this points directly to that element. Otherwise, it points to a
+    // real ArrayList object.
+    private static Object listOrSingleEl;
+   static void addToListNew(Foo foo) {
+        if (listOrSingleEl == null) { // Empty list
+            listOrSingleEl = foo;
+        } else if (listOrSingleEl instanceof Foo) { // Single-element
+            Foo firstEl = (Foo) listOrSingleEl;
+            ArrayList<Foo> list = new ArrayList<>();
+            listOrSingleEl = list;
+            list.add(firstEl);
+            list.add(foo);
+        } else { // Real, multiple-element list
+            ((ArrayList<Foo>) listOrSingleEl).add(foo);
+        }
     }
 
-    private static void addToList(Object element) {
+    public static void main(String[] args) {
 
-        if(list == null){
-            list.add((Integer)element);
-        }else if(element instanceof Integer){
+        Foo foo = new Foo();
 
-        }else{
 
-        }
+        addToListOld(foo);
+        addToListNew(foo);
+    }
 
+
+    static class Foo{
 
     }
 }
