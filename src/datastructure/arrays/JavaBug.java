5cadca9a3e1a81a8b9578f5c47bdff68e98035cd
charanjits <charanjit.singh90@gmail.com>
2018-07-02 15:44:07 +0530

    Added Array

---
+++ src/datastructure/arrays/JavaBug.java
@@ -0,0 +1,25 @@
+package datastructure.arrays;
+
+import java.util.Arrays;
+
+public class JavaBug {
+
+    public static void main(String[] args) {
+        call1();
+
+    }
+
+
+    private static void call1() {
+        int size = 3;
+        String[] array = new String[size];
+        Arrays.fill(array, "");
+        for(int i = 0; i <= 100; ) {
+            array[i++%size] += i + " ";
+        }
+        for(String element: array) {
+            System.out.println(element);
+        }
+    }
+
+}
